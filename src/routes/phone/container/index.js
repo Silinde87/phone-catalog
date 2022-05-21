bf93657d6ac8b17c13e9db7767f65f13b3b1e701
Pau Rodriguez <paurodriguezmolina@gmail.com>
2022-05-21 19:43:32 +0200

    feat: implement delete phone and return to home

--- src/routes/phone/container/index.js
+++ src/routes/phone/container/index.js
@@ -3,17 +3,19 @@ import { useNavigate } from 'react-router-dom';
 import { useReactContext } from '../../../context/Context';
 import { ROUTES } from '../../../globals/constants';
 import Phone from '../component/Phone';
+import PhoneService from '../../../services/PhoneService';
 
 const PhonePage = () => {
-  const { phonesState } = useReactContext();
+  const { phonesState, setPhonesState } = useReactContext();
+  const { selectedPhone, phones } = phonesState;
   const navigate = useNavigate();
 
   useEffect(() => {
     // Redirect to home if detail page is not accessed through home
-    if (!phonesState.selectedPhone) {
+    if (!selectedPhone) {
       navigate(ROUTES.HOME);
     }
-  }, [phonesState.selectedPhone]);
+  }, [selectedPhone]);
 
   const handleBackClick = () => {
     navigate(-1);
@@ -22,12 +24,24 @@ const PhonePage = () => {
     //console.log('edit');
   };
   const handleDeleteClick = () => {
-    //console.log('remove');
+    PhoneService.deletePhone(selectedPhone.id)
+      .then(() => {
+        const newPhones = phones.filter((phone) => phone.id !== selectedPhone.id);
+        setPhonesState((prevState) => ({
+          ...prevState,
+          phones: newPhones,
+          selectedPhone: null,
+        }));
+        navigate(ROUTES.HOME);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
     <Phone
-      selectedPhone={phonesState.selectedPhone}
+      selectedPhone={selectedPhone}
       handleBackClick={handleBackClick}
       handleEditClick={handleEditClick}
       handleDeleteClick={handleDeleteClick}
